7dc4d350cae53850f53d6b3f9ae81c55ce831c75
bhandekunal16 <bhandekunal16@gmail.com>
2023-11-07 16:23:20 +0530

    update

--- src/constant/response.ts
+++ src/constant/response.ts
@@ -1,5 +1,7 @@
+import { STATUS_CODES } from 'http';
+
 export class response {
-  public static SUCCESS = 'SUCCESS';
-  public static ERROR = 'error';
-  public static FAILURE = 'FAILURE';
+  public static SUCCESS = 'SUCCESS' + ' -' + STATUS_CODES[200];
+  public static ERROR = 'error' + ' -' + STATUS_CODES[403];
+  public static FAILURE = 'FAILURE' + ' -' + STATUS_CODES[404];
 }
