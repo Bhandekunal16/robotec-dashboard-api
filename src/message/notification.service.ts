cfdafcc7c8f0c4dabf7b699b7098074201a8f932
bhandekunal16 <bhandekunal16@gmail.com>
2023-12-15 16:30:16 +0530

    update

--- src/message/notification.service.ts
+++ src/message/notification.service.ts
@@ -1,19 +1,18 @@
-// notification.service.ts
+
 
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+
 const apiUrl = 'https://mailer-service-eight.vercel.app/message/send-email';
 
 @Injectable()
 export class NotificationService {
   async sendEmailNotification(to: string, message: string) {
-    axios
-      .post(apiUrl, { to, message })
-      .then((response) => {
-        console.log('Response:', response.data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+    try {
+      const response = await axios.post(apiUrl, { to, message });
+      console.log('Response:', response.data);
+    } catch (error) {
+      console.error('Error:', error.message || error);
+    }
   }
 }
