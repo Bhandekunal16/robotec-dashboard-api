4e2f141ba005b46deed91869e961e38fa799e65d
bhandekunal16 <bhandekunal16@gmail.com>
2023-12-15 15:51:06 +0530

    update

--- src/data/descriptor/descriptor.module.ts
+++ src/data/descriptor/descriptor.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { Encrypt } from './descriptor';
 import { DescriptorController } from './descriptor.controller';
+import { ValidationService } from '../validation/validation.service';
 
 @Module({
-  providers: [Encrypt],
+  providers: [Encrypt, ValidationService],
   controllers: [DescriptorController],
 })
 export class DescriptorModule {}
