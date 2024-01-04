7ad12738176169a188427a6cb554e940836dbff5
bhandekunal16 <bhandekunal16@gmail.com>
2024-01-04 09:36:48 +0530

    Changed the import statement for the Logger module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { Neo4jModule } from 'nest-neo4j/dist';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
